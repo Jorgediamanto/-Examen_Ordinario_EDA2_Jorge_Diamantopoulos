7b46c7a5e156eaf13abb347e4261e2be10ced710
Jorge <jorgediamanto01@gmail.com>
2023-01-20 00:04:42 +0100

    $$

---
+++ main.py
@@ -0,0 +1,10 @@
+from Ej1 y Ej2 import stormtrooper
+from Ej3 import artefactosvaliosos
+from Ej4 import usar_la_fuerza
+from Ej5 import ejercicio_5
+from Ej6 import ejercicio_6
+from Ej7 import ejercicio_7
+from Ej8 import ejercicio_8
+
+if __name__ == "__main__":
+    print('Ejercicio 1 y 2')
